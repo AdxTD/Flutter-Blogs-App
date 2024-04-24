02b8c7f59f51c04842f572da340412dedee446b1
TahaDarwishIT <taha.darwish@admix.in>
2024-04-24 23:24:46 +0200

    implemented datasource for blogs

---
+++ lib/features/blog/data/datasources/blog_remote_data_source.dart
@@ -0,0 +1,54 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:io';
+
+import 'package:blog_app/core/error/exceptions.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+import 'package:blog_app/features/blog/data/model/BlogModel.dart';
+
+abstract interface class BlogRemoteDataSource {
+  Future<BlogModel> uploadBlog(BlogModel blogModel);
+  Future<String> uploadBlogImage({
+    required File image,
+    required BlogModel blog,
+  });
+}
+
+class BlogRemoteDataSourceImpl implements BlogRemoteDataSource {
+  final SupabaseClient supabaseClient;
+
+  BlogRemoteDataSourceImpl({
+    required this.supabaseClient,
+  });
+
+  @override
+  Future<BlogModel> uploadBlog(BlogModel blogModel) async {
+    try {
+      final blogData = await supabaseClient
+          .from('blogs')
+          .insert(blogModel.toJson())
+          .select();
+      return BlogModel.fromJson(blogData.first);
+    } catch (e) {
+      throw ServerException(e.toString());
+    }
+  }
+
+  @override
+  Future<String> uploadBlogImage({
+    required File image,
+    required BlogModel blog,
+  }) async {
+    try {
+      await supabaseClient.storage.from('blog_images').upload(
+            blog.id,
+            image,
+          );
+      return supabaseClient.storage.from('blog_images').getPublicUrl(
+            blog.id,
+          );
+    } catch (e) {
+      throw ServerException(e.toString());
+    }
+  }
+}
---
+++ lib/features/blog/data/model/BlogModel.dart
@@ -0,0 +1,39 @@
+import 'package:blog_app/features/blog/domain/entities/blog.dart';
+
+class BlogModel extends Blog {
+  BlogModel({
+    required super.id,
+    required super.posterId,
+    required super.title,
+    required super.content,
+    required super.imageUrl,
+    required super.topics,
+    required super.updatedAt,
+  });
+
+  Map<String, dynamic> toJson() {
+    return <String, dynamic>{
+      'id': id,
+      'poster_id': posterId,
+      'title': title,
+      'content': content,
+      'image_url': imageUrl,
+      'topics': topics,
+      'update_at': updatedAt.toIso8601String(),
+    };
+  }
+
+  factory BlogModel.fromJson(Map<String, dynamic> map) {
+    return BlogModel(
+      id: map['id'] as String,
+      posterId: map['poster_id'] as String,
+      title: map['title'] as String,
+      content: map['content'] as String,
+      imageUrl: map['image_url'] as String,
+      topics: List<String>.from(map['topics'] ?? []),
+      updatedAt: map['update_at'] == null
+          ? DateTime.now()
+          : DateTime.parse(map['update_at']),
+    );
+  }
+}
