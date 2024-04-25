5fd919b86ea5c22763022ce5d8972a0753abd4d5
TahaDarwishIT <taha.darwish@admix.in>
2024-04-25 16:35:24 +0200

    implemented bloc for uploading blogs

---
+++ lib/features/blog/presentation/bloc/blog_bloc.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+
+import 'package:blog_app/features/blog/domain/usecase/upload_blog.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'blog_event.dart';
+part 'blog_state.dart';
+
+class BlogBloc extends Bloc<BlogEvent, BlogState> {
+  final UploadBlog _uploadBlog;
+  BlogBloc({required UploadBlog uploadBlog})
+      : _uploadBlog = uploadBlog,
+        super(BlogInitial()) {
+    on<BlogEvent>((event, emit) => emit(BlogLoading()));
+    on<BlogUpload>(_onBlogUpload);
+  }
+
+  void _onBlogUpload(BlogUpload event, Emitter<BlogState> emit) async {
+    final res = await _uploadBlog(UploadBlogParams(
+      image: event.image,
+      title: event.title,
+      content: event.content,
+      posterId: event.posterId,
+      topics: event.topics,
+    ));
+    res.fold(
+      (l) => emit(BlogFailure(l.message)),
+      (r) => emit(BlogSuccess()),
+    );
+  }
+}
---
+++ lib/features/blog/presentation/bloc/blog_event.dart
@@ -0,0 +1,22 @@
+part of 'blog_bloc.dart';
+
+@immutable
+sealed class BlogEvent {
+  const BlogEvent();
+}
+
+final class BlogUpload extends BlogEvent {
+  final File image;
+  final String title;
+  final String content;
+  final String posterId;
+  final List<String> topics;
+
+  const BlogUpload({
+    required this.image,
+    required this.title,
+    required this.content,
+    required this.posterId,
+    required this.topics,
+  });
+}
